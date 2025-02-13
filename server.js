b024c62ba3bccab179c334784d186745cb802176
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-13 13:19:41 +0545

    Token id fetched and saved user to the db

--- server.js
+++ server.js
@@ -25,6 +25,51 @@ app.get("/", async (req, res) => {
   res.redirect(GOOGLE_OAUTH_CONSENT_SCREEN_URL);
 });
 
+// =========================================== User Model ============================================
+
+import mongoose from "mongoose";
+
+import jwt from "jsonwebtoken";
+
+const mongoDBURI = process.env.MONGO_DB_URI;
+
+mongoose.connect(mongoDBURI);
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, "Please provide a  name"],
+    minlength: 3,
+    maxlength: 56,
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email.",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: false,
+  },
+});
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  return token;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+// =========================================== User Model ============================================
+
 // Handle the OAuth 2.0 server response
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
@@ -56,11 +101,28 @@ app.get("/google/callback", async (req, res) => {
 
   console.log(id_token);
 
-//   Verify and extract the information in the id token
-const token_info_response = await fetch(
+  //   Verify and extract the information in the id token
+  //   const token_info_response = await fetch(
+  //     `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
+  //   );
+  //   res.status(token_info_response.status).json(await token_info_response.json());
+
+  const token_info_response = await fetch(
     `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
-)
-res.status(token_info_response.status).json(await token_info_response.json())
+  );
+
+  const token_info_data = await token_info_response.json(); // <-- Fix: Store response
+
+  console.log("Token Info Data:", token_info_data);
+
+  // Use the infor in google id token to manage user authentication
+  const { email, name } = token_info_data;
+  let user = await User.findOne({ email }).select("-password");
+  if (!user) {
+    user = await User.create({ email, name });
+  }
+  const token = user.generateToken();
+  res.status(token_info_response.status).json({ user, token });
 });
 
 const port = process.env.PORT || 8000;
