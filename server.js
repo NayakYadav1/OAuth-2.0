d867b2cf2025aebc79eeca5981f40bf92ce4fe3c
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-13 11:50:36 +0545

    Authorization Server created

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const fetch = require('node-fetch');
+const dotenv = require("dotenv");
+const express = require("express");
+const fetch = require("node-fetch");
 
 dotenv.config();
 
 const app = express();
 
-app.use (express.json());
+app.use(express.json());
+
+const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
+const GOOGLE_OAUTH_SCOPES = [
+  "https%3A//www.googleapis.com/auth/userinfo.email",
+
+  "https%3A//www.googleapis.com/auth/userinfo.profile",
+];
 
 app.get("/", async (req, res) => {
-    res.send("Sign in with Google");
-})
+  const state = "some_state";
+  const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+  const GOOGLE_OAUTH_CONSENT_SCREEN_URL = `${GOOGLE_OAUTH_URL} ?
+  client_id = ${GOOGLE_CLIENT_ID} & redirect_uri = ${GOOGLE_CALLBACK_URL} & accress_type = offline&response_type = code&state = ${state} &scope = ${scopes}`;
+  res.redirect(GOOGLE_OAUTH_CONSENT_SCREEN_URL);
+});
 
-app.get('/google/callback', async (req, res) => {
-    res.send("Google OAuth Callback Url!");
-})
+app.get("/google/callback", async (req, res) => {
+  res.send("Google OAuth Callback Url!");
+});
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
