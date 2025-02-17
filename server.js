e598223cc6742ff22135de26965e825b1af15820
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-17 21:18:09 +0545

    Db and logger created

--- server.js
+++ server.js
@@ -1,132 +1,26 @@
-import * as dotenv from "dotenv";
 import express from "express";
-import fetch from "node-fetch";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import morgan from "morgan";
+import logger from "./config/logger.js";
 
+// Load environment variables
 dotenv.config();
 
+// Initialize app
 const app = express();
-
 app.use(express.json());
+app.use(morgan("combined", { stream: logger.stream }));
 
-// Redirect the user to the Google OAuth Consent Screen
-const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
-const GOOGLE_OAUTH_SCOPES = [
-  "https%3A//www.googleapis.com/auth/userinfo.email",
+// Connect to MongoDB
+connectDB();
 
-  "https%3A//www.googleapis.com/auth/userinfo.profile",
-];
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
-app.get("/", async (req, res) => {
-  const state = "some_state";
-  const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
-  const GOOGLE_OAUTH_CONSENT_SCREEN_URL = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&accress_type=offline&response_type=code&state=${state}&scope=${scopes}`;
-  res.redirect(GOOGLE_OAUTH_CONSENT_SCREEN_URL);
-});
-
-// =========================================== User Model ============================================
-
-import mongoose from "mongoose";
-
-import jwt from "jsonwebtoken";
-
-const mongoDBURI = process.env.MONGO_DB_URI;
-
-mongoose.connect(mongoDBURI);
-
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: [true, "Please provide a  name"],
-    minlength: 3,
-    maxlength: 56,
-  },
-  email: {
-    type: String,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email.",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    minlength: 6,
-    required: false,
-  },
-});
-
-UserSchema.methods.generateToken = function () {
-  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
-  return token;
-};
-
-const User = mongoose.model("User", UserSchema);
-
-// =========================================== User Model ============================================
-
-// Handle the OAuth 2.0 server response
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
-
-app.get("/google/callback", async (req, res) => {
-  console.log(req.query);
-
-  const { code } = req.query;
-
-  const data = {
-    code,
-    client_id: GOOGLE_CLIENT_ID,
-    client_secret: GOOGLE_CLIENT_SECRET,
-    redirect_uri: "http://localhost:8000/google/callback",
-    grant_type: "authorization_code",
-  };
-
-  console.log(data);
-
-  //   exchange authorization code for access token & id_token
-  const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
-
-  const access_token_data = await response.json();
-
-  const { id_token } = access_token_data;
-
-  console.log(id_token);
-
-  //   Verify and extract the information in the id token
-  //   const token_info_response = await fetch(
-  //     `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
-  //   );
-  //   res.status(token_info_response.status).json(await token_info_response.json());
-
-  const token_info_response = await fetch(
-    `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
-  );
-
-  const token_info_data = await token_info_response.json();
-
-  console.log("Token Info Data:", token_info_data);
-
-  // Use the infor in google id token to manage user authentication
-  const { email, name } = token_info_data;
-  let user = await User.findOne({ email }).select("-password");
-  if (!user) {
-    user = await User.create({ email, name });
-  }
-  const token = user.generateToken();
-  res.status(token_info_response.status).json({ user, token });
-});
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Server listening
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
