8c36a3427c0108fad3b8d4cb6a1afa97801dd1e1
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-18 10:23:16 +0545

    Models and db con

---
+++ models/Product.js
@@ -0,0 +1,27 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  reviews: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
