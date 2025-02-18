7c7654a30922ff642c275316ad2e3d27b0c23520
NayakYadav1 <yadavnayak202@gmail.com>
2025-02-18 10:21:14 +0545

    Whole code refactored

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { googleAuth, google } from "../controllers/authController.js";
-
-const router = express.Router();
-// Route to initiate Google OAuth
-router.get("/google", google);
-router.get("/google/callback", googleAuth);
-export default router;
